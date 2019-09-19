c20fee39e623a8f7defd4b70c1912ae3b71b8d00
RKhalilKelly <55443399+RKhalilKelly@users.noreply.github.com>
2019-09-19 08:41:01 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/main.cpp
@@ -0,0 +1,23 @@
+// Robert Khalil Kelly
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>  
+
+int main()
+{
+	std::cout << "Robert Khalil Kelly" << std::endl
+			  << "Period 1: Intro to Computer Science" << std::endl
+		      << "Period 2: Global History and Geography" << std::endl
+		      << "Period 3: Physical Education" << std::endl
+		      << "Period 4: Earth Science" << std::endl
+		      << "Period 5: Spanish 1" << std::endl
+		      << "Period 6: Geometry" << std::endl
+		      << "Period 7: Health" << std::endl
+		      << "Period 8: Lunch" << std::endl
+		      << "Period 9: English Honors 10" << std::endl
+		      << "Period 10: House" << std::endl
+}
+
